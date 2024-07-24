f8f976b655fcbfb15a99daefd553bb7b47b94220
Dima Hutsol <35511017+dimahutsol@users.noreply.github.com>
2024-07-24 10:38:25 +0100

    project init

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './js/requests';
+import './js/header';
+import './js/hero';
+import './js/about';
+import './js/benefits';
+import './js/projects';
+import './js/faq';
+import './js/covers';
+import './js/reviews';
+import './js/footer';

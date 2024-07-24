b356c86bc4725abe9d8ec44a80df006edeebd137
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-07-24 21:21:11 +0300

    Add mobile menu and js code for click btn and add style css

---
+++ src/js/header.js
@@ -0,0 +1,11 @@
+const headerMobileMenuDiv = document.querySelector('.header-mobile-menu');
+const headerNavMenuBtn = document.querySelector('.header-nav-menu');
+const headerBtnCloseMenu = document.querySelector('.header-btn-close-menu');
+
+headerNavMenuBtn.addEventListener('click', () => {
+    headerMobileMenuDiv.classList.add('header-mob-open');
+});
+
+headerBtnCloseMenu.addEventListener('click', () => { 
+    headerMobileMenuDiv.classList.remove('header-mob-open');
+})

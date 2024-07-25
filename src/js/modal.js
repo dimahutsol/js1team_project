2fcdcb64adc76651ade1537c5244137c89b85534
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-07-25 21:59:53 +0300

    section modal

---
+++ src/js/modal.js
@@ -0,0 +1,28 @@
+const refs = {
+  closeModalBtn: document.querySelector('.btnCloseModal'),
+  backdrop: document.querySelector('.backdrop'),
+};
+
+refs.closeModalBtn.addEventListener('click', () => {
+  closeModal();
+});
+
+window.addEventListener('click', e => {
+  if (e.target === refs.backdrop) {
+    closeModal();
+  }
+});
+
+window.addEventListener('keydown', e => {
+  if (e.key === 'Escape') {
+    closeModal();
+  }
+});
+
+function showModal() {
+  refs.backdrop.classList.add('is-open');
+}
+
+function closeModal() {
+  refs.backdrop.classList.remove('is-open');
+}

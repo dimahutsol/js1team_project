272bddfaf91007f714929efdd80241863f1069bf
Dima Hutsol <35511017+dimahutsol@users.noreply.github.com>
2024-07-25 17:28:33 +0100

    update

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,27 +1,27 @@
 const section = {
-    cards: document.querySelectorAll('.marquee__card'),
-    covers: document.querySelector('.covers'),
+  cards: document.querySelectorAll('.marquee__card'),
+  covers: document.querySelector('.covers'),
 };
 
-let options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0,
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0,
 };
 
-let animationCovers = entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            section.cards.forEach(card => {
-                card.classList.add('animation');
-            });
-            return;
-        }
-        section.cards.forEach(card => {
-            card.classList.remove('animation');
-        });
+const animationCovers = entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      section.cards.forEach(card => {
+        card.classList.add('animation');
+      });
+      return;
+    }
+    section.cards.forEach(card => {
+      card.classList.remove('animation');
     });
+  });
 };
 
-let observer = new IntersectionObserver(animationCovers, options);
+const observer = new IntersectionObserver(animationCovers, options);
 observer.observe(section.covers);

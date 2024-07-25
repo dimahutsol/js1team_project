6e81209b16db664bf18172d2e2d6152bea344a9e
dekizber <dekizber@gmail.com>
2024-07-25 17:45:37 +0300

    add js

---
+++ src/js/covers.js
@@ -0,0 +1,27 @@
+const section = {
+    cards: document.querySelectorAll('.marquee__card'),
+    covers: document.querySelector('.covers'),
+};
+
+let options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0,
+};
+
+let animationCovers = entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            section.cards.forEach(card => {
+                card.classList.add('animation');
+            });
+            return;
+        }
+        section.cards.forEach(card => {
+            card.classList.remove('animation');
+        });
+    });
+};
+
+let observer = new IntersectionObserver(animationCovers, options);
+observer.observe(section.covers);

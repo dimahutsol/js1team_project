c5ffac6748606b76091cada205f5d8752d5f0239
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-07-25 20:34:50 +0300

    updated footer.js

--- src/js/footer.js
+++ src/js/footer.js
@@ -10,4 +10,25 @@ refs.formFooter.addEventListener('submit', async function (e) {
   console.log(formData);
   const email = formData.get('email');
   const comment = formData.get('comment');
+
+  try {
+    const response = await fetch('https://portfolio-js.b.goit.study/api-docs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, comment }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Server error');
+    }
+
+    refs.formFooter.reset();
+    showModal();
+  } catch (error) {
+    alert(
+      'Error: ' + error.message + '. Please check your input and try again.'
+    );
+  }
 });

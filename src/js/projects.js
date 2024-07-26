9f991c90b67ac16cd830942b45bb16aacf28ca4b
Dima Hutsol <35511017+dimahutsol@users.noreply.github.com>
2024-07-26 15:57:43 +0100

    update

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,33 +1,26 @@
-// Import Swiper bundle with all modules installed
 import Swiper from 'swiper/bundle';
-// Import Swiper styles bundle
 import 'swiper/css/bundle';
 
-import mobimage1x from '../img/Projects/proj_1template_mob@1x.webp';
-import mobimage2x from '../img/Projects/proj_1template_mob@2x.webp';
-import tabimage2x from '../img/Projects/proj_1template_tab@2x.webp';
-import descimage1x from '../img/Projects/proj_1template_desk@1x.webp';
-import tabimage1x from '../img/Projects/proj_1template_tab@1x.webp';
-import descimage2x from '../img/Projects/proj_1template_desk@2x.webp';
+import mobimage1_1x from '../img/Projects/proj_1template_mob@1x.webp';
+import mobimage1_2x from '../img/Projects/proj_1template_mob@2x.webp';
+import tabimage1_2x from '../img/Projects/proj_1template_tab@2x.webp';
+import descimage1_1x from '../img/Projects/proj_1template_desk@1x.webp';
+import tabimage1_1x from '../img/Projects/proj_1template_tab@1x.webp';
+import descimage1_2x from '../img/Projects/proj_1template_desk@2x.webp';
 
-import mobimage1x from '../img/Projects/proj_2template_mob@1x.webp';
-import mobimage2x from '../img/Projects/proj_2template_mob@2x.webp';
-import tabimage2x from '../img/Projects/proj_2template_tab@1x.webp';
-import descimage1x from '../img/Projects/proj_2template_tab@2x.webp';
-import tabimage1x from '../img/Projects/proj_2template_desk@1x.webp';
-import descimage2x from '../img/Projects/proj_2template_desk@2x.webp';
+import mobimage2_1x from '../img/Projects/proj_2template_mob@1x.webp';
+import mobimage2_2x from '../img/Projects/proj_2template_mob@2x.webp';
+import tabimage2_2x from '../img/Projects/proj_2template_tab@1x.webp';
+import descimage2_1x from '../img/Projects/proj_2template_tab@2x.webp';
+import tabimage2_1x from '../img/Projects/proj_2template_desk@1x.webp';
+import descimage2_2x from '../img/Projects/proj_2template_desk@2x.webp';
 
-import mobimage1x from '../img/Projects/proj_3template_mob@1x.webp';
-import mobimage2x from '../img/Projects/proj_3template_mob@2x.webp';
-import tabimage2x from '../img/Projects/proj_3template_tab@2x.webp';
-import descimage1x from '../img/Projects/proj_3template_desk@1x.webp';
-import tabimage1x from '../img/Projects/proj_3template_tab@1x.webp';
+import mobimage3_1x from '../img/Projects/proj_3template_mob@1x.webp';
+import mobimage3_2x from '../img/Projects/proj_3template_mob@2x.webp';
+import tabimage3_2x from '../img/Projects/proj_3template_tab@2x.webp';
+import descimage3_1x from '../img/Projects/proj_3template_desk@1x.webp';
+import tabimage3_1x from '../img/Projects/proj_3template_tab@1x.webp';
 import descimage3_2x from '../img/Projects/proj_3template_desk@2x.webp';
-
-console.log(mobimage1x);
-
-const imageUrl = mobimage1x.replace('1template_mob@1x.webp', '');
-// console.log('🚀 ~ imageUrl:', imageUrl);
 
 const refs = {
   projectListRef: document.querySelector('.project-list-js'),
@@ -36,10 +29,12 @@ const refs = {
   nextSlideProjBtn: document.querySelector('.projects-navigation-next'),
 };
 
+let isProjectSwiperInViewPort = false;
+
 const projSwiper = new Swiper(refs.projectWrapper, {
   slidesPerView: 1,
   spaceBetween: 25,
-  effect: 'cube', // Set the effect to 'cube'
+  effect: 'cube',
 
   centerInsufficientSlides: true,
   centeredSlides: false,
@@ -69,8 +64,6 @@ const projSwiper = new Swiper(refs.projectWrapper, {
   },
 });
 
-let isProjectSwiperInViewPort = false;
-
 projSwiper.on('keyPress', (swiper, keyCode) => {
   if (keyCode === 9 && isProjectSwiperInViewPort) {
     swiper.slideNext();
@@ -94,94 +87,72 @@ const observer = new IntersectionObserver(
   }
 );
 observer.observe(refs.projectWrapper);
+
 const projectArray = [
   {
     title: 'Programming Across Borders: Ideas, Technologies, Innovations',
     tags: ['react', 'js', 'node js', 'git'],
     links: 'https://github.com/dimahutsol/js1team_project/tree/dev',
-    mobimage1x,
-    mobimage2x,
-    tabimage1x,
-    tabimage2x,
-    descimage1x,
-    descimage2x,
+    imgMob_1x: mobimage1_1x,
+    imgMob_2x: mobimage1_2x,
+    imgTab_1x: tabimage1_1x,
+    imgTab_2x: tabimage1_2x,
+    imgDesc_1x: descimage1_1x,
+    imgDesc_2x: descimage1_2x,
   },
   {
     title: 'Programming Across Borders: Ideas, Technologies, Innovations',
     tags: ['react', 'js', 'node js', 'git'],
     links: 'https://github.com/dimahutsol/js1team_project/tree/dev',
-    mobimage1x,
-    mobimage2x,
-    tabimage1x,
-    tabimage2x,
-    descimage1x,
-    descimage2x,
+    imgMob_1x: mobimage2_1x,
+    imgMob_2x: mobimage2_2x,
+    imgTab_1x: tabimage2_1x,
+    imgTab_2x: tabimage2_2x,
+    imgDesc_1x: descimage2_1x,
+    imgDesc_2x: descimage2_2x,
   },
   {
     title: 'Programming Across Borders: Ideas, Technologies, Innovations',
     tags: ['react', 'js', 'node js', 'git'],
     links: 'https://github.com/dimahutsol/js1team_project/tree/dev',
-    mobimage1x,
-    mobimage2x,
-    tabimage1x,
-    tabimage2x,
-    descimage1x,
-    descimage2x,
+    imgMob_1x: mobimage3_1x,
+    imgMob_2x: mobimage3_2x,
+    imgTab_1x: tabimage3_1x,
+    imgTab_2x: tabimage3_2x,
+    imgDesc_1x: descimage3_1x,
+    imgDesc_2x: descimage3_2x,
   },
-  // {
-  //   title: 'Programming Across Borders: Ideas, Technologies, Innovations',
-  //   tags: ['react', 'js', 'node js', 'git'],
-  //   links: 'https://github.com/dimahutsol/js1team_project/tree/dev',
-  //   mobimage1x: `${imageUrl}1template_mob@1x.webp`,
-  //   mobimage2x: `${imageUrl}1template_mob@2x.webp`,
-  //   tabimage1x: `${imageUrl}1template_tab@1x.webp`,
-  //   tabimage2x: `${imageUrl}1template_tab@2x.webp`,
-  //   descimage1x: `${imageUrl}1template_desk@1x.webp`,
-  //   descimage2x: `${imageUrl}1template_desk@2x.webp`,
-  // },
-  // {
-  //   title: 'Programming Across Borders: Ideas, Technologies, Innovations',
-  //   tags: ['react', 'js', 'node js', 'git'],
-  //   links: 'https://github.com/dimahutsol/js1team_project/tree/dev',
-  //   mobimage21x: `${imageUrl}2template_mob@1x.webp`,
-  //   mobimage22x: `${imageUrl}2template_mob@2x.webp`,
-  //   tabimage21x: `${imageUrl}2template_tab@1x.webp`,
-  //   tabimage22x: `${imageUrl}2template_tab@2x.webp`,
-  //   descimage21x: `${imageUrl}2template_desk@1x.webp`,
-  //   descimage22x: `${imageUrl}2template_desk@2x.webp`,
-  // },
-  // {
-  //   title: 'Programming Across Borders: Ideas, Technologies, Innovations',
-  //   tags: ['react', 'js', 'node js', 'git'],
-  //   links: 'https://github.com/dimahutsol/js1team_project/tree/dev',
-  //   mobimage31x: `${imageUrl}3template_mob@1x.webp`,
-  //   mobimage32x: `${imageUrl}3template_mob@2x.webp`,
-  //   tabimage31x: `${imageUrl}3template_tab@1x.webp`,
-  //   tabimage32x: `${imageUrl}3template_tab@2x.webp`,
-  //   descimage31x: `${imageUrl}3template_desk@1x.webp`,
-  //   descimage32x: `${imageUrl}3template_desk@2x.webp`,
-  // },
 ];
 
-function createProjectExample(project) {
-  const tags = project.tags
+function createProjectExample({
+  title,
+  tags,
+  links,
+  imgMob_1x,
+  imgMob_2x,
+  imgTab_1x,
+  imgTab_2x,
+  imgDesc_1x,
+  imgDesc_2x,
+}) {
+  const newTags = tags
     .map(tag => `<li class="tag-item"><p class="tag-text">#${tag}</p></li>`)
     .join('');
 
   return `
-    <li class="swiper-slide" tabindex="0">
+    <li class="swiper-slide">
       <div class="projects-li-item">
         <div class="projects-white-wrapper">
-          <ul class="tag-list">${tags}</ul>
-          <h3 class="projects-header">${project.title}</h3>
-          <a class="project-btn" type="button" href="${project.links}" target="_blank">See project</a>
+          <ul class="tag-list">${newTags}</ul>
+          <h3 class="projects-header">${title}</h3>
+          <a class="project-btn" type="button" href="${links}" target="_blank">See project</a>
         </div>
         <div class="project-image-wrapper">
           <picture>
-            <source media="(min-width: 1440px)" srcset="${project.descimage1x} 1x, ${project.descimage2x} 2x" type="image/webp">
-            <source media="(min-width: 768px)" srcset="${project.tabimage1x} 1x, ${project.tabimage2x} 2x" type="image/webp">
-            <source media="(max-width: 767px)" srcset="${project.mobimage1x} 1x, ${project.mobimage2x} 2x" type="image/webp">
-            <img src="${project.mobimage1x}" alt="project example image" loading="lazy" class="project-image">
+            <source media="(min-width: 1440px)" srcset="${imgDesc_1x} 1x, ${imgDesc_2x} 2x" type="image/webp">
+            <source media="(min-width: 768px)" srcset="${imgTab_1x} 1x, ${imgTab_2x} 2x" type="image/webp">
+            <source media="(max-width: 767px)" srcset="${imgMob_1x} 1x, ${imgMob_2x} 2x" type="image/webp">
+            <img src="${imgMob_1x}" alt="project example image" loading="lazy" class="project-image">
           </picture>
         </div>
       </div>

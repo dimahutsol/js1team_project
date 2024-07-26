78b5e12176c038da950ea92cb07cb1a67866a7fc
Nicolai Dodeac <Mykola.Dodiak@gmail.com>
2024-07-26 12:02:02 +0100

    swiper

--- src/js/projects.js
+++ src/js/projects.js
@@ -2,7 +2,10 @@
 import Swiper from 'swiper/bundle';
 // Import Swiper styles bundle
 import 'swiper/css/bundle';
-
+import mobimage1x from '../img/Projects/proj_1template_mob@1x.png';
+console.log(mobimage1x);
+const imageUrl = mobimage1x.replace('1template_mob@1x.png', '');
+console.log('🚀 ~ imageUrl:', imageUrl);
 const refs = {
   projectListRef: document.querySelector('.project-list-js'),
   projectWrapper: document.querySelector('.project-swiper'),
@@ -13,11 +16,42 @@ const refs = {
 const projSwiper = new Swiper(refs.projectWrapper, {
   slidesPerView: 1,
   spaceBetween: 25,
+  effect: 'cube', // Set the effect to 'cube'
+  // Enables slide shadows
   centerInsufficientSlides: true,
   centeredSlides: false,
   centeredSlidesBounds: false,
   watchSlidesVisibility: true,
-  keyboard: { enabled: true },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true, // Enable keyboard control only when Swiper is in viewport
+    pageUpDown: true, // Enable control via Page Up and Page Down keys
+  },
+
+  keyPress: (swiper, keyCode, event) => {
+    if (keyCode === 9) {
+      // Tab key
+      if (event.shiftKey) {
+        // Shift + Tab -> Navigate to previous slide
+        if (!swiper.isBeginning) {
+          swiper.slidePrev();
+        }
+      } else {
+        // Tab -> Navigate to next slide
+        if (!swiper.isEnd) {
+          swiper.slideNext();
+        }
+      }
+    }
+  },
+
+  cubeEffect: {
+    shadow: true, // Enables main slider shadow
+    shadowOffset: 20, // Main shadow offset in px
+    shadowScale: 0.94, // Main shadow scale ratio
+    slideShadows: true,
+  },
 
   pagination: {
     el: '.projects-swiper-pagination',
@@ -29,47 +63,12 @@ const projSwiper = new Swiper(refs.projectWrapper, {
   },
 });
 
-function updateNavigation() {
-  if (projSwiper.isBeginning) {
-    refs.prevSlideProjBtn.classList.add('projects-navigation-btn-disabled');
-    refs.prevSlideProjBtn.setAttribute('disabled', true);
-    refs.prevSlideProjBtn
-      .querySelector('svg')
-      .classList.add('projects-navigation-icon-disabled');
-  } else {
-    refs.prevSlideProjBtn.classList.remove('projects-navigation-btn-disabled');
-    refs.prevSlideProjBtn.removeAttribute('disabled');
-    refs.prevSlideProjBtn
-      .querySelector('svg')
-      .classList.remove('projects-navigation-icon-disabled');
-  }
-
-  if (projSwiper.isEnd) {
-    refs.nextSlideProjBtn.classList.add('projects-navigation-btn-disabled');
-    refs.nextSlideProjBtn.setAttribute('disabled', true);
-    refs.nextSlideProjBtn
-      .querySelector('svg')
-      .classList.add('projects-navigation-icon-disabled');
-  } else {
-    refs.nextSlideProjBtn.classList.remove('projects-navigation-btn-disabled');
-    refs.nextSlideProjBtn.removeAttribute('disabled');
-    refs.nextSlideProjBtn
-      .querySelector('svg')
-      .classList.remove('projects-navigation-icon-disabled');
-  }
-}
-
-projSwiper.on('slideChange', updateNavigation);
-projSwiper.on('reachBeginning', updateNavigation);
-projSwiper.on('reachEnd', updateNavigation);
-projSwiper.on('init', updateNavigation);
-
 const projectArray = [
   {
     title: 'Programming Across Borders: Ideas, Technologies, Innovations',
     tags: ['react', 'js', 'node js', 'git'],
     links: 'https://github.com/dimahutsol/js1team_project/tree/dev',
-    mobimage1x: './img/Projects/proj_1template_mob@1x.png',
+    mobimage1x: `${imageUrl}1template_mob@1x.png`,
     mobimage2x: './img/Projects/proj_1template_mob@2x.png',
     tabimage1x: './img/Projects/proj_1template_tab@1x.png',
     tabimage2x: './img/Projects/proj_1template_tab@2x.png',
@@ -115,7 +114,7 @@ function createProjectExample(project) {
         <a class="project-btn" type="button" href="${project.links}">See project</a>
       </div>
       <div class="project-image-wrapper">
-        <img class="project-image" 
+        <img class="project-image" loading="lazy"
              src="${project.mobimage1x}"
              srcset="${project.mobimage1x} 1x, ${project.mobimage2x} 2x, 
                      ${project.tabimage1x} 1x, ${project.tabimage2x} 2x, 
@@ -124,6 +123,43 @@ function createProjectExample(project) {
       </div>
       </div>
     </li>`;
+}
+{
+  /* <picture>
+  <source
+    media="(min-width: 1440px)"
+    srcset="
+                ${item.imgTab1} 1x,
+                ${item.imgTab2} 2x
+                "
+    type="image/webp"
+  />
+  <source
+    media="(min-width: 768px)"
+    srcset="
+                ${item.imgTab1} 1x,
+                ${item.imgTab2} 2x
+                "
+    type="image/webp"
+  />
+  <source
+    media="(max-width: 767px)"
+    srcset="
+                ${item.imgMob1} 1x,
+                ${item.imgMob2} 2x
+                "
+    type="image/webp"
+  />
+  <img
+    src="${item.imgMob1}"
+    alt="project"
+    width="311"
+    height="195"
+    class="projects-card-img"
+  /> */
+}
+{
+  /* </picture>; */
 }
 
 function projectList(arr) {

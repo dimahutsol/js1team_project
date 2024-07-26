74781ad49418e55fb9b79627975fdb0b7d09314a
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-07-26 18:47:47 +0300

    summary

--- src/js/footer.js
+++ src/js/footer.js
@@ -4,7 +4,29 @@ import { showModal } from './modal';
 const refs = {
   formFooter: document.querySelector('.js-form-footer'),
   sendBtnFooter: document.querySelector('.js-btn-footer'),
+  emailInput: document.querySelector('.js-email-input'),
+  emailMessage: document.querySelector('.js-email-message'),
 };
+
+function validateEmail() {
+  const pattern = /^\w+(\.\w+)?@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+  const email = refs.emailInput.value;
+  if (pattern.test(email)) {
+    refs.emailInput.classList.remove('invalid');
+    refs.emailInput.classList.add('valid');
+    refs.emailMessage.textContent = 'Success!';
+    refs.emailMessage.classList.remove('error');
+    refs.emailMessage.classList.add('success');
+  } else {
+    refs.emailInput.classList.remove('valid');
+    refs.emailInput.classList.add('invalid');
+    refs.emailMessage.textContent = 'Invalid email, try again';
+    refs.emailMessage.classList.remove('success');
+    refs.emailMessage.classList.add('error');
+  }
+}
+
+refs.emailInput.addEventListener('input', validateEmail);
 
 axios.defaults.baseURL = 'https://portfolio-js.b.goit.study';
 

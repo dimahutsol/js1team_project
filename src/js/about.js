8e7145e308b87a83c60f8898888681bde0907c45
gramatchi <nik98.champion@gmail.com>
2024-07-27 19:10:32 +0200

    Nichita/26 07 24/about me (#40)
    * Tablet version
    
    * Desctopp scc
    
    * finish 27/08

--- src/js/about.js
+++ src/js/about.js
@@ -1,14 +1,19 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
+import Swiper from 'swiper';
+import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
+import 'swiper/css';
+import image from '/img/symbols.svg';
 
 const refs = {
-  listRef: document.querySelector('.about-me-ac-list'),
+  listAcRef: document.querySelector('.about-me-ac-list'),
+  listSkillsRef: document.querySelector('.about-me-skills-list'),
 };
 
 const aboutMeList = [
   {
     title: 'About me',
-    svgPath: './img/symbols.svg#arrow-up',
+    svgPath: `${image}#arrow-up`,
     content: `
         <p class="about-me-desc">
           I am Lloyd Jefferson, a talented programmer with extensive expertise in software development. 
@@ -16,7 +21,7 @@ const aboutMeList = [
           I always follow the latest trends and look for unconventional, creative solutions to problems. 
           I have a high level of analytical skills and am able to effectively solve even the most difficult tasks encountered on the way.
         </p>
-        <p class="about-me-desc">
+        <p class="about-me-desc-2">
           Able to work both independently and in a team. I can effectively cooperate with colleagues, exchanging ideas and finding optimal solutions. 
           Professional maturity allows you to calmly cope with challenges and stressful situations, while maintaining a high quality of work. 
           I am always looking for opportunities for self-improvement. I actively study new technologies and practices to stay abreast of the latest innovations. 
@@ -26,7 +31,7 @@ const aboutMeList = [
   },
   {
     title: 'Role',
-    svgPath: './img/symbols.svg#arrow-down',
+    svgPath: `${image}#arrow-down`,
     content: `
         <p class="about-me-role-text">Frontend development</p>
         <p class="about-me-role-text">HeadlessCMS,Wordpress</p>
@@ -35,7 +40,7 @@ const aboutMeList = [
   },
   {
     title: 'Education',
-    svgPath: './img/symbols.svg#arrow-down',
+    svgPath: `${image}#arrow-down`,
     content: `
         <p class="about-me-education-text">
           2018 - 2019 / Frontend Development Diploma, GoIT IT School, New York
@@ -48,6 +53,16 @@ const aboutMeList = [
         </p>
       `,
   },
+];
+
+const aboutMeSkillsList = [
+  'HTML/CSS',
+  'JavaScript',
+  'React',
+  'Node.js',
+  'React Native',
+  'Soft Skills',
+  'Python',
 ];
 
 function listItemTemplate({ title, svgPath, content }) {
@@ -74,7 +89,7 @@ function listTemplate(arr) {
 
 function renderList(arr) {
   const markup = listTemplate(arr);
-  refs.listRef.insertAdjacentHTML('beforeend', markup);
+  refs.listAcRef.insertAdjacentHTML('beforeend', markup);
 }
 
 renderList(aboutMeList);
@@ -95,3 +110,79 @@ document.querySelectorAll('.about-me-ac-button').forEach(button => {
     this.querySelector('.about-me-slider-arrow').classList.toggle('rotate-180');
   });
 });
+
+function listSkillItemTemplate(nameSkill) {
+  return `
+    <li class="about-me-skills-item swiper-slide">
+      <p>${nameSkill}</p>
+    </li>
+  `;
+}
+
+function renderSkillList(arr) {
+  const markup = arr.map(skill => listSkillItemTemplate(skill)).join('');
+  refs.listSkillsRef.insertAdjacentHTML('beforeend', markup);
+}
+
+renderSkillList(aboutMeSkillsList);
+
+const skillsCarousel = new Swiper('.about-me-skills-swiper', {
+  modules: [Navigation, Keyboard, Mousewheel],
+  navigation: {
+    nextEl: '.about-me-swiper-button-next',
+    grabCursor: true,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  mousewheel: {
+    invert: true,
+  },
+
+  breakpoints: {
+    375: {
+      slidesPerView: 2,
+      width: 260,
+    },
+
+    768: {
+      slidesPerView: 3,
+      width: 600,
+    },
+
+    1440: {
+      slidesPerView: 6,
+      width: 1200,
+    },
+  },
+
+  loop: true,
+  setWrapperSize: true,
+  spaceBetween: 0,
+  speed: 600,
+  simulateTouch: false,
+  slideToClickedSlide: true,
+  slidesPerGroup: 1,
+});
+
+function updateHighlight() {
+  const slides = skillsCarousel.slides;
+  slides.forEach(slide => slide.classList.remove('is-first'));
+  const firstVisibleSlide = slides[skillsCarousel.activeIndex];
+  firstVisibleSlide.classList.add('is-first');
+}
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'Tab') {
+    event.preventDefault();
+    skillsCarousel.slideNext(600);
+  }
+});
+
+skillsCarousel.update();
+skillsCarousel.on('slideChange', updateHighlight);
+
+skillsCarousel.on('slideChangeTransitionEnd', updateHighlight);
+
+updateHighlight();

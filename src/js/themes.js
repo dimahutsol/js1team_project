48a012fec8c4e6949dc0816a80c5faeb08ef3021
Dima Hutsol <35511017+dimahutsol@users.noreply.github.com>
2024-07-29 20:37:15 +0100

    Update themes.js

--- src/js/themes.js
+++ src/js/themes.js
@@ -43,9 +43,9 @@ window.addEventListener('scroll', function () {
   }
 });
 
-document.addEventListener('DOMContentLoaded', checkStorageonPageLoad);
+document.addEventListener('DOMContentLoaded', checkStorageOnPageLoad);
 
-function checkStorageonPageLoad(e) {
+function checkStorageOnPageLoad(e) {
   try {
     const theme = JSON.parse(localStorage.getItem(THEME_STATE));
 

14e1844bb167e0c05c80de90984057c618e8744f
Dima Hutsol <35511017+dimahutsol@users.noreply.github.com>
2024-07-29 18:32:02 +0100

    bug fix

--- src/js/about.js
+++ src/js/about.js
@@ -122,7 +122,7 @@ document.querySelectorAll('.ac-trigger').forEach(trigger => {
 
 function listSkillItemTemplate(nameSkill) {
   return `
-    <li class="about-me-skills-item swiper-slide button-animation">
+    <li class="about-me-skills-item swiper-slide block-animation">
       <p>${nameSkill}</p>
     </li>
   `;
